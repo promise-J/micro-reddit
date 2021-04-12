e53a5edcce66deec8f97b99bf1037c431fbe8d76
Promise Johnson <chiemelapromise30@gmail.com>
2021-04-13 06:02:19 +1300

    Add the User model and migrate done too.

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :email, presence: true, length: { minimum: 9 }, uniqueness: true
+    validates :password, presence: true, length: {minimum: 8 }, uniqueness: true
+end
